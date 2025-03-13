e94be18e622a203ce3e1a7463a6b1770bcf4ca86
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-13 08:41:45 +0500

    Space given

--- src/app/components/Category/index.tsx
+++ src/app/components/Category/index.tsx
@@ -3,22 +3,22 @@ import React from 'react'
 // Category component displays a list of similar job categories
 const Category = () => {
     const Buttons = [
-        {btnName: 'Frontend'},
-        {btnName: 'Backend'},
-        {btnName: 'Graphic Designer'},
-    ] 
-  return (
-    <div className='flex items-center space-x-3 text-textClr3'>
-        <p>Similar:</p>
-        <div className='text-xs sm:text-base flex space-x-3'>
-        {Buttons.map((v)=><>
-        <button
-        className='border border-textClr3 bg-transparent p-1 px-2 sm:px-4 rounded-md'
-        >{v.btnName}</button>
-        </>)}
+        { btnName: 'Frontend' },
+        { btnName: 'Backend' },
+        { btnName: 'Graphic Designer' },
+    ]
+    return (
+        <div className='flex items-center space-x-3 text-textClr3'>
+            <p>Similar:</p>
+            <div className='text-xs sm:text-base flex space-x-3'>
+                {Buttons.map((v) => <>
+                    <button
+                        className='border border-textClr3 bg-transparent p-1 px-2 sm:px-4 rounded-md'
+                    >{v.btnName}</button>
+                </>)}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Category
